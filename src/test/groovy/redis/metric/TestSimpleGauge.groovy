d780cf7989db097c2c4d59f8d1065be299f6133f
kerry <dingyong87@163.com>
2024-05-24 18:33:52 +0800

    test: add simple gauge test.

---
+++ src/test/groovy/redis/metric/TestSimpleGauge.groovy
@@ -0,0 +1,20 @@
+package redis.metric
+
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.exporter.common.TextFormat
+
+def g = new SimpleGauge('test', 'test', 'slot')
+def labelValues = ['0']
+def labelValues2 = ['1']
+
+g.register()
+g.setRawGetter {
+    Map<String, SimpleGauge.ValueWithLabelValues> map = [:]
+    map.put('a', new SimpleGauge.ValueWithLabelValues(1.0, labelValues))
+    map.put('b', new SimpleGauge.ValueWithLabelValues(2.0, labelValues2))
+    map
+}
+
+def sw = new StringWriter()
+TextFormat.write004(sw, CollectorRegistry.defaultRegistry.metricFamilySamples())
+println sw.toString()
