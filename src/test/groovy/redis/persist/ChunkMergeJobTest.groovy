b88511fd6badaddf6ef43d24e8b612b5284db7ee
kerry <dingyong87@163.com>
2024-05-29 23:43:21 +0800

    style: naming.

--- src/test/groovy/redis/persist/ChunkMergeJobTest.groovy
+++ src/test/groovy/redis/persist/ChunkMergeJobTest.groovy
@@ -9,20 +9,23 @@ import spock.lang.Specification
 class ChunkMergeJobTest extends Specification {
     def 'merge segments'() {
         given:
-
+        final byte slot = 0
+        final int bucketIndex = 0
+        final int segmentIndex = 0
+        final int walGroupIndex = 0
         def snowFlake = new SnowFlake(1, 1)
-        def segmentBatch = new SegmentBatch((byte) 0, snowFlake)
 
+        and:
         var list = Mock.prepareValueList(100)
 
         int[] nextNSegmentIndex = [0, 1, 2, 3, 4, 5, 6]
         ArrayList<PersistValueMeta> returnPvmList = []
 
+        def segmentBatch = new SegmentBatch(slot, snowFlake)
         def r = segmentBatch.splitAndTight(list, nextNSegmentIndex, returnPvmList)
         ArrayList<PersistValueMeta> somePvmList = returnPvmList[0..<10]
 
         and:
-
         System.setProperty('jnr.ffi.asm.enabled', 'false')
         def libC = LibraryLoader.create(LibC.class).load('c')
 
@@ -34,44 +37,42 @@ class ChunkMergeJobTest extends Specification {
         def fdReadWriteForChunkSegments = new FdReadWrite('chunk_data_index_0', libC, chunkDataFile)
         fdReadWriteForChunkSegments.initByteBuffers(true)
 
-        fdReadWriteForChunkSegments.writeSegment(0, r[0].tightBytesWithLength, false)
-        println 'write segment 0'
+        fdReadWriteForChunkSegments.writeSegment(segmentIndex, r[0].tightBytesWithLength, false)
+        println 'write segment ' + segmentIndex
 
         def keyBucketsDataFile = new File(Consts.slotDir, 'key-bucket-split-0.dat')
         def fdReadWriteForKeyLoader = new FdReadWrite('key_loader_data', libC, keyBucketsDataFile)
         fdReadWriteForKeyLoader.initByteBuffers(false)
         // clear old data
         // segment index -> bucket index
-        fdReadWriteForKeyLoader.writeSegment(0, new byte[4096], false)
+        fdReadWriteForKeyLoader.writeSegment(bucketIndex, new byte[4096], false)
 
         and:
-
-        System.setProperty('jnr.ffi.asm.enabled', 'false')
-        def keyLoader = new KeyLoader((byte) 0, ConfForSlot.global.confBucket.bucketsPerSlot, Consts.slotDir, snowFlake)
+        def keyLoader = new KeyLoader(slot, ConfForSlot.global.confBucket.bucketsPerSlot, Consts.slotDir, snowFlake)
         keyLoader.fdReadWriteArray = [fdReadWriteForKeyLoader]
 
-        keyLoader.metaKeyBucketSplitNumber = new MetaKeyBucketSplitNumber((byte) 0, Consts.slotDir)
-        keyLoader.metaKeyBucketSplitNumber.set(0, (byte) 1)
-        keyLoader.statKeyCountInBuckets = new StatKeyCountInBuckets((byte) 0, keyLoader.bucketsPerSlot, Consts.slotDir)
+        keyLoader.metaKeyBucketSplitNumber = new MetaKeyBucketSplitNumber(slot, Consts.slotDir)
+        keyLoader.metaKeyBucketSplitNumber.set(bucketIndex, (byte) 1)
+        keyLoader.statKeyCountInBuckets = new StatKeyCountInBuckets(slot, keyLoader.bucketsPerSlot, Consts.slotDir)
 
-        keyLoader.updatePvmListBatchAfterWriteSegments(0, somePvmList, false)
-        println 'bucket 0 key count: ' + keyLoader.getKeyCountInBucketIndex(0)
+        keyLoader.updatePvmListBatchAfterWriteSegments(walGroupIndex, somePvmList, false)
+        println 'bucket ' + bucketIndex + ' key count: ' + keyLoader.getKeyCountInBucketIndex(bucketIndex)
 
         when:
 
-        def wal = new Wal((byte) 0, 0, null, null, snowFlake)
-        def oneSlot = new OneSlot((byte) 0, Consts.slotDir, keyLoader, wal)
-        oneSlot.metaChunkSegmentFlagSeq.setSegmentMergeFlag(0, Chunk.SEGMENT_FLAG_REUSE_AND_PERSISTED, 1L)
+        def wal = new Wal(slot, walGroupIndex, null, null, snowFlake)
+        def oneSlot = new OneSlot(slot, Consts.slotDir, keyLoader, wal)
+        oneSlot.metaChunkSegmentFlagSeq.setSegmentMergeFlag(segmentIndex, Chunk.SEGMENT_FLAG_REUSE_AND_PERSISTED, 1L)
 
-        var chunk = new Chunk((byte) 0, snowFlake, Consts.slotDir, oneSlot, keyLoader, null)
+        var chunk = new Chunk(slot, snowFlake, Consts.slotDir, oneSlot, keyLoader, null)
         chunk.fdReadWriteArray = [fdReadWriteForChunkSegments]
         oneSlot.chunk = chunk
 
-        var chunkMergeWorker = new ChunkMergeWorker((byte) 0, oneSlot)
+        var chunkMergeWorker = new ChunkMergeWorker(slot, oneSlot)
 
-        ArrayList<Integer> needMergeSegmentIndexList = [0]
-        def job = new ChunkMergeJob((byte) 0, needMergeSegmentIndexList, chunkMergeWorker, keyLoader.snowFlake)
-        job.testTargetBucketIndex = 0
+        ArrayList<Integer> needMergeSegmentIndexList = [segmentIndex]
+        def job = new ChunkMergeJob(slot, needMergeSegmentIndexList, chunkMergeWorker, snowFlake)
+        job.testTargetBucketIndex = bucketIndex
         job.mergeSegments(needMergeSegmentIndexList)
 
         then:
