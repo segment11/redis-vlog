ea30620603b7db344766f769858dc86d3f4cfc5e
kerry <dingyong87@163.com>
2024-06-19 15:07:47 +0800

    test: given one 100% cover test sample.

--- src/test/groovy/redis/decode/RequestTest.groovy
+++ src/test/groovy/redis/decode/RequestTest.groovy
@@ -20,8 +20,11 @@ class RequestTest extends Specification {
         !request.isRepl()
 
         request.cmd() == 'set'
+        request.cmd() == 'set'
         request.singleSlot == Request.SLOT_CAN_HANDLE_BY_ANY_WORKER
         request.slotNumber == 1
+
+        println request.toString()
 
         when:
         request.slotWithKeyHashList = [BaseCommand.slot('key'.bytes, 1)]
@@ -29,6 +32,24 @@ class RequestTest extends Specification {
         then:
         request.slotWithKeyHashList.size() == 1
         request.singleSlot == request.slotWithKeyHashList[0].slot
+
+        when:
+        request.slotWithKeyHashList = [null]
+
+        then:
+        request.singleSlot == Request.SLOT_CAN_HANDLE_BY_ANY_WORKER
+
+        when:
+        byte[][] dataRepl = new byte[3][]
+        dataRepl[0] = new byte[8]
+        dataRepl[1] = new byte[1]
+        dataRepl[1][0] = (byte) 0
+
+        def requestRepl = new Request(dataRepl, false, true)
+
+        then:
+        requestRepl.isRepl()
+        requestRepl.singleSlot == 0
 
         when:
         byte[][] data2 = new byte[3][]
@@ -45,10 +66,10 @@ class RequestTest extends Specification {
         ]
 
         def isMultiSlot = request2.slotWithKeyHashList.collect { it.slot }.unique().size() > 1
-        request2.isCrossRequestWorker = isMultiSlot
+        request2.setCrossRequestWorker(isMultiSlot)
 
         then:
-        request2.isCrossRequestWorker == isMultiSlot
+        request2.isCrossRequestWorker() == isMultiSlot
         request2.singleSlot == request2.slotWithKeyHashList[0].slot
 
         when:
@@ -59,7 +80,15 @@ class RequestTest extends Specification {
         request3.checkCmdIfCrossRequestWorker()
 
         then:
-        request3.isCrossRequestWorker
+        request3.isCrossRequestWorker()
+
+        when:
+        data3[0] = 'xxxx'.bytes
+        def request33 = new Request(data3, false, false)
+        request33.checkCmdIfCrossRequestWorker()
+
+        then:
+        !request33.isCrossRequestWorker()
 
         when:
         Request.crossRequestWorkerCmdList << 'xxxx'
@@ -70,6 +99,6 @@ class RequestTest extends Specification {
         request4.checkCmdIfCrossRequestWorker()
 
         then:
-        request4.isCrossRequestWorker
+        request4.isCrossRequestWorker()
     }
 }
