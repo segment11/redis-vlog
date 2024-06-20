c34a925d65617123e5482ed59e35b047f1183c88
kerry <dingyong87@163.com>
2024-06-20 21:51:27 +0800

    test: improve task chain unit test coverage.

--- src/test/groovy/redis/task/TaskChainTest.groovy
+++ src/test/groovy/redis/task/TaskChainTest.groovy
@@ -11,6 +11,9 @@ class TaskChainTest extends Specification {
 
         @Override
         void run() {
+            if (loopCount == 8) {
+                throw new RuntimeException('test exception')
+            }
             println 'task1, loop count: ' + loopCount
         }
 
@@ -20,28 +23,46 @@ class TaskChainTest extends Specification {
         void setLoopCount(int loopCount) {
             this.loopCount = loopCount
         }
+
+        @Override
+        int executeOnceAfterLoopCount() {
+            2
+        }
+    }
+
+    static class Task2 extends Task1 {
+        @Override
+        String name() {
+            'task2'
+        }
     }
 
     def 'test all'() {
         given:
         def taskChain = new TaskChain()
         def task1 = new Task1()
+        def task2 = new Task2()
 
         when:
         taskChain.add(task1)
+        taskChain.add(task1)
+        taskChain.add(task2)
 
         then:
-        taskChain.list.size() == 1
+        taskChain.list.size() == 2
 
         when:
+        taskChain.remove('task2')
+
         10.times {
             taskChain.doTask(it)
         }
 
         then:
-        task1.loopCount == 9
+        task1.loopCount == 8
 
         when:
+        taskChain.remove('task1')
         taskChain.remove('task1')
 
         then:
