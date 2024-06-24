7422688a387f338a3025b442d74becad841e9ac1
kerry <dingyong87@163.com>
2024-06-24 15:02:24 +0800

    test: add unit test.

---
+++ src/test/groovy/redis/CompressStatsTest.groovy
@@ -0,0 +1,42 @@
+package redis
+
+import spock.lang.Specification
+
+class CompressStatsTest extends Specification {
+    def 'test all'() {
+        given:
+        def compressStats = new CompressStats('test')
+
+        when:
+        compressStats.compressedCount = 0
+        compressStats.decompressedCount = 0
+
+        def mfsList = compressStats.compressStatsGauge.collect()
+
+        then:
+        mfsList[0].samples.size() == 0
+
+        when:
+        compressStats.rawCount = 2
+        compressStats.compressedCount = 1
+        compressStats.rawTotalLength = 100
+
+        compressStats.decompressedCount = 0
+
+        mfsList = compressStats.compressStatsGauge.collect()
+
+        then:
+        mfsList[0].samples.size() == 7
+
+        when:
+        compressStats.compressedCount = 0
+
+        compressStats.decompressedCount = 10
+        compressStats.decompressedCostTimeTotalUs = 50
+
+        mfsList = compressStats.compressStatsGauge.collect()
+
+        then:
+        mfsList[0].samples.size() == 3
+    }
+}
