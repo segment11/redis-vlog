f06fdd519a3f0649eebac7b15adba43ca254e7c2
kerry <dingyong87@163.com>
2024-06-25 17:43:54 +0800

    test: add one command 100% coverage unit test.

--- src/test/groovy/redis/command/AGroupTest.groovy
+++ src/test/groovy/redis/command/AGroupTest.groovy
@@ -2,10 +2,13 @@ package redis.command
 
 import redis.BaseCommand
 import redis.mock.InMemoryGetSet
+import redis.reply.ErrorReply
+import redis.reply.IntegerReply
+import redis.reply.NilReply
 import spock.lang.Specification
 
 class AGroupTest extends Specification {
-    def "ParseSlot"() {
+    def 'test parse slot'() {
         given:
         byte[][] dataAppend = new byte[3][]
         int slotNumber = 128
@@ -15,15 +18,57 @@ class AGroupTest extends Specification {
 
         when:
         def slotWithKeyHash = AGroup.parseSlot('append', dataAppend, slotNumber)
+        def slotWithKeyHashList = AGroup.parseSlots('append', dataAppend, slotNumber)
 
         then:
         slotWithKeyHash.slot() == 63
+        slotWithKeyHashList.size() == 1
+
+        when:
+        slotWithKeyHash = AGroup.parseSlot('appendx', dataAppend, slotNumber)
+
+        then:
+        slotWithKeyHash == null
+
+        when:
+        byte[][] dataWrongSize = new byte[2][]
+        slotWithKeyHash = AGroup.parseSlot('append', dataWrongSize, slotNumber)
+
+        then:
+        slotWithKeyHash == null
     }
 
-    def "Append"() {
+    def 'test handle'() {
         given:
-        byte[][] dataAppend = new byte[3][]
         def inMemoryGetSet = new InMemoryGetSet()
+
+        byte[][] data1 = new byte[3][]
+        def aGroup1 = new AGroup('append', data1, null)
+        aGroup1.byPassGetSet = inMemoryGetSet
+        aGroup1.from(BaseCommand.mockAGroup((byte) 0, (byte) 1, (short) 1))
+
+        when:
+        data1[1] = 'a'.bytes
+        data1[2] = '123'.bytes
+
+        def reply = aGroup1.handle()
+
+        then:
+        reply instanceof IntegerReply
+
+        when:
+        def aGroupNotCmdMatch = new AGroup('appendx', data1, null)
+
+        reply = aGroupNotCmdMatch.handle()
+
+        then:
+        reply == NilReply.INSTANCE
+    }
+
+    def 'test append'() {
+        given:
+        def inMemoryGetSet = new InMemoryGetSet()
+        byte[][] dataAppend = new byte[3][]
 
         and:
         dataAppend[1] = 'a'.bytes
@@ -45,5 +90,16 @@ class AGroupTest extends Specification {
 
         then:
         aGroup.get('a'.bytes) == '123456'.bytes
+
+        when:
+        byte[][] dataWrongSize = new byte[2][]
+        def aGroup2 = new AGroup('append', dataWrongSize, null)
+//        aGroup2.byPassGetSet = inMemoryGetSet
+//        aGroup2.from(BaseCommand.mockAGroup((byte) 0, (byte) 1, (short) 1))
+
+        def reply = aGroup2.append()
+
+        then:
+        reply == ErrorReply.FORMAT
     }
 }
