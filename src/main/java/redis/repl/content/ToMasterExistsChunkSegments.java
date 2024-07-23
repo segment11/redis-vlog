fdec84b53b9df73170af4278d6a3f5e293e87137
kerry <dingyong87@163.com>
2024-07-23 20:14:09 +0800

    refactor: slave get master exists data and binlog replay.

---
+++ src/main/java/redis/repl/content/ToMasterExistsChunkSegments.java
@@ -0,0 +1,47 @@
+package redis.repl.content;
+
+import io.activej.bytebuf.ByteBuf;
+import redis.repl.ReplContent;
+
+import java.nio.ByteBuffer;
+
+public class ToMasterExistsChunkSegments implements ReplContent {
+    private final int beginSegmentIndex;
+    private final int segmentCount;
+    private final byte[] metaBytes;
+
+    public ToMasterExistsChunkSegments(int beginSegmentIndex, int segmentCount, byte[] metaBytes) {
+        this.beginSegmentIndex = beginSegmentIndex;
+        this.segmentCount = segmentCount;
+        this.metaBytes = metaBytes;
+    }
+
+    @Override
+    public void encodeTo(ByteBuf toBuf) {
+        toBuf.writeInt(beginSegmentIndex);
+        toBuf.writeInt(segmentCount);
+        toBuf.write(metaBytes);
+    }
+
+    @Override
+    public int encodeLength() {
+        // begin segment index int, segment count int, meta bytes
+        return 4 + 4 + metaBytes.length;
+    }
+
+    // 4K one chunk segment, 1024 chunk segments means read 4M data and send to slave
+    public static final int REPL_ONCE_CHUNK_SEGMENT_COUNT = 1024;
+
+    // once compare with 1024 chunk segments
+    public static boolean isSlaveSameForThisBatch(byte[] metaBytesMaster, byte[] contentBytesFromSlave) {
+        // content bytes include begin segment index int, segment count int
+        if (contentBytesFromSlave.length != metaBytesMaster.length + 8) {
+            throw new IllegalArgumentException("Repl exists chunk segments meta from slave meta length is not equal to master meta length");
+        }
+
+        var bufferFromSlave = ByteBuffer.wrap(contentBytesFromSlave, 8, contentBytesFromSlave.length - 8).slice();
+        var bufferMaster = ByteBuffer.wrap(metaBytesMaster);
+
+        return bufferFromSlave.equals(bufferMaster);
+    }
+}
