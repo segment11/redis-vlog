aaac220ca339e86f06140609452c5a3fd63c291f
kerry <dingyong87@163.com>
2024-07-24 16:50:03 +0800

    test: add one slot unit test.

---
+++ src/test/groovy/redis/persist/OneSlotTest.groovy
@@ -0,0 +1,51 @@
+package redis.persist
+
+import io.activej.eventloop.Eventloop
+import spock.lang.Specification
+
+import java.time.Duration
+
+class OneSlotTest extends Specification {
+    def 'test mock'() {
+        given:
+        final byte slot = 0
+
+        def oneSlot = new OneSlot(slot, Consts.slotDir, null, null)
+
+        when:
+        oneSlot.metaChunkSegmentFlagSeq.cleanUp()
+        oneSlot.metaChunkSegmentIndex.cleanUp()
+
+        def eventloopCurrent = Eventloop.builder()
+                .withCurrentThread()
+                .withIdleInterval(Duration.ofMillis(100))
+                .build()
+        var oneSlot2 = new OneSlot(slot, eventloopCurrent)
+        oneSlot2.threadIdProtectedForSafe = Thread.currentThread().threadId()
+        def call = oneSlot2.asyncCall(() -> 1)
+        def run = oneSlot2.asyncRun { println 'async run' }
+        eventloopCurrent.run()
+        then:
+        call.result
+        run != null
+
+        when:
+        def eventloop = Eventloop.builder()
+                .withIdleInterval(Duration.ofMillis(100))
+                .build()
+        eventloop.keepAlive(true)
+        var oneSlot3 = new OneSlot(slot, eventloop)
+        Thread.start {
+            eventloop.run()
+        }
+        call = oneSlot3.asyncCall(() -> 1)
+        run = oneSlot3.asyncRun { println 'async run' }
+        eventloopCurrent.run()
+        then:
+        call.whenResult { result -> result == 1 }.result
+        run != null
+
+        cleanup:
+        eventloop.breakEventloop()
+    }
+}
